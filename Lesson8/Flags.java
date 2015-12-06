46869fc0966e966651ac0229933329f70e5f9437
ZRonchy <chi.ronchy.zhang@gmail.com>
2015-12-06 15:45:07 +0000

    refactored the file

--- Lesson8/Flags.java
+++ Lesson8/Flags.java
@@ -47,55 +47,45 @@
  * expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
  * Elements of input arrays can be modified.
  */
- 
+
+//use binary search to search through all the possible flags.
 import java.util.Arrays;
 import java.lang.Integer;
 import java.util.ArrayList;
 import java.util.List;
 class Solution {
-    public int solution(int[] A) 
-    {
+    public int solution(int[] A) {
         ArrayList<Integer> array = new ArrayList<Integer>();  
-            for (int i = 1; i < A.length - 1; i++) 
-            {  
-                if (A[i - 1] < A[i] && A[i + 1] < A[i]) 
-                {  
-                    array.add(i);  
-                }  
-            }  
-       if (array.size() == 1 || array.size() == 0) 
-       {  
-            return array.size();  
-       }  
+		for (int i = 1; i < A.length - 1; i++) {  
+			if (A[i - 1] < A[i] && A[i + 1] < A[i]) {  
+				array.add(i);  
+			}  
+		}  
+	    if (array.size() == 1 || array.size() == 0) {  
+			return array.size();  
+	    }  
         int sf = 1;  
         int ef = array.size();  
         int result = 1;  
-        while (sf <= ef) 
-        {  
+        while (sf <= ef) {  
             int flag = (sf + ef) / 2;  
             boolean suc = false;  
             int used = 0;  
             int mark = array.get(0);  
-            for (int i = 0; i < array.size(); i++) 
-            {  
-                if (array.get(i) >= mark) 
-                {  
+            for (int i = 0; i < array.size(); i++) {  
+                if (array.get(i) >= mark) {  
                     used++;  
                     mark = array.get(i) + flag;  
-                        if (used == flag) 
-                        {                       
-                            suc = true;  
-                            break;  
-                        }  
+					if (used == flag) {                       
+						suc = true;  
+						break;  
+					}  
                 }  
             }  
-            if (suc) 
-            {  
+            if (suc) {  
                 result = flag;  
                 sf = flag + 1;  
-            } 
-            else 
-            {  
+            }else {  
                 ef = flag - 1;  
             }  
         }  
